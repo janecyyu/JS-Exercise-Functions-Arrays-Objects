561878e769b11b1301587531f31215661607d256
janecyyu <janecyyu@gmail.com>
2020-02-25 11:01:55 -0800

    fix format on inventory

--- index.js
+++ index.js
@@ -135,12 +135,7 @@ function get3rdCar(inventory) {
 var inventory = [
   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-  {
-    id: 3,
-    car_make: "Land Rover",
-    car_model: "Defender Ice Edition",
-    car_year: 2010
-  },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
